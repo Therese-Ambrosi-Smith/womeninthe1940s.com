680d9a7d2c5c3a261f236357ac75355bb485f36f
Aleksandr Zapparov <alexander@zapparov.dev>
2024-02-27 20:25:20 +0100

    updated layouts, added redirects via scripts-head, init 404 page, clean up issues

--- src/content/blog/blog.11tydata.js
+++ src/content/blog/blog.11tydata.js
@@ -1,7 +1,7 @@
 module.exports = {
 	tags: ["posts"],
 	layout: "post.njk",
-	permalink: "/blog/{{ title | slugify }}/",
+	permalink: "/{{ title | slugify }}/",
     author: "Therese",
     authorUrl: "https://thereseambrosismith.com/"
 };
